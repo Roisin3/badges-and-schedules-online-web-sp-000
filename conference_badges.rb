17fe56803569a16f93d771d9fd64d06c0c4c7817
Michael Lynch <michael@michaelrlynch.com>
2019-08-12 01:51:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,9 +5,11 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
+  badge_names = []
   attendees.each do |name|
-    puts "Hello my name is #{name}."
+    badge_names << "Hello my name is #{name}."
   end
+  puts badge names
 end
 
 def assign_rooms
